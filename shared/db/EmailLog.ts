a8787c510bc3209ea316c78697d7db341fba955e
notquitenothing <nqn@notquitenothing.com>
2025-07-27 10:52:11 -0500

    Added 'approved' email that is sent when a user becomes approved for the first time.

--- shared/db/EmailLog.ts
+++ shared/db/EmailLog.ts
@@ -2,7 +2,7 @@ import type { Audit } from './Audit'
 
 export type EmailLog = Pick<Audit, 'createdAt'> & {
   id: string
-  type: 'email_verification' | 'password_reset' | 'invitation' | 'admin_notification'
+  type: 'email_verification' | 'password_reset' | 'invitation' | 'admin_notification' | 'approved'
   toUser?: string | null
   reasons?: string | null
 
