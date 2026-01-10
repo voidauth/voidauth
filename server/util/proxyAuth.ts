1eba7e04e8efe368044de20516f05736511ab3f6
notquitenothing <nqn@notquitenothing.com>
2026-01-09 22:01:42 -0600

    Minor changes to proxyauth domain formatting logic. Added logging to proxyauth domain forbidden response.

--- server/util/proxyAuth.ts
+++ server/util/proxyAuth.ts
@@ -8,6 +8,7 @@ import { ADMIN_GROUP } from '@shared/constants'
 import { loginFactors } from '@shared/user'
 import { userCanLogin } from './auth'
 import { getSession } from '../oidc/provider'
+import { logger } from './logger'
 
 // proxy auth common
 export async function proxyAuth(url: URL, method: 'forward-auth' | 'auth-request', req: Request, res: Response) {
@@ -92,7 +93,24 @@ export async function proxyAuth(url: URL, method: 'forward-auth' | 'auth-request
 
   // Check user groups for access if not an admin
   if (!user.groups.some(g => g.name === ADMIN_GROUP)) {
-    if (!match || (match.groups.length && !user.groups.some(g => match.groups.includes(g.name)))) {
+    if (!match) {
+      const logInfo = {
+        reason: 'no_domain_match',
+        url: url.href,
+        urlDomain: formattedUrl,
+      }
+      logger.debug(`proxyauth forbidden: ${JSON.stringify(logInfo)}`)
+      res.sendStatus(403)
+      return
+    } else if (match.groups.length && !user.groups.some(g => match.groups.includes(g.name))) {
+      const logInfo = {
+        reason: 'no_required_group',
+        url: url.href,
+        urlDomain: formattedUrl,
+        domain: match.domain,
+        domainGroups: match.groups,
+      }
+      logger.debug(`proxyauth forbidden: ${JSON.stringify(logInfo)}`)
       res.sendStatus(403)
       return
     }
